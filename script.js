c50ea91865f09005d2b1651db5a746b31c97b6cd
NaruChigo31 <volomaksim72@gmail.com>
2023-02-16 18:54:18 +0200

    asd
    agfadfgsfs

--- script.js
+++ script.js
@@ -3,52 +3,20 @@ function createElem(tag, properties = [], styles = [], parentElem){
     let elem = document.createElement(tag);
 
     parentElemPosition = document.querySelector(parentElem);
-    document.body.parentElemPosition.append(elem);
+    parentElemPosition.append(elem);
      
     for (let propertie of properties) {
-
-        if(propertie[0] === "innerHTML") {
-            elem.innerHTML = propertie[1];
-        }
-
-        if(propertie[0] === "type") {
-            elem.type = propertie[1];
-        }
-
-        if(propertie[0] === "placeholder") {
-            elem.placeholder = propertie[1];
-        }
+        elem[propertie[0]] = propertie[1]
     };
     
-    // for (let styleType of styles) {
-    //     elem.style.styleType[0] = styleType[1]
-    // };
-    // чет не работает
-
-    for (let styleType of styles){
-
-        if (styleType[0] === "color"){
-            elem.style.color = styleType[1]
-        }
-
-        if (styleType[0] === "backgroundColor"){
-            elem.style.backgroundColor = styleType[1]
-        }
-
-        if (styleType[0] === "width"){
-            elem.style.width = styleType[1]
-        }
-
-        if (styleType[0] === "height"){
-            elem.style.height = styleType[1]
-        }
-        if (styleType[0] === "fontSize"){
-            elem.style.fontSize = styleType[1]
-        }
+    for (let styleType of styles) {
+        elem.style[styleType[0]] = styleType[1]
     }
+    
 }
 
+createElem("input", [["innerHTML", "Lorem ipsum"],["placeholder","pizza"]], [["fontSize", "18px"],["color","red"]], ".coolDiv");
 
-createElem("input", [["innerNTML", "Lorem ipsum"],["placeholder","gde seichas"]], [["fontSize", "18px"],["color","red"]], ".coolDiv")
+let a = document.querySelector(input)
 
-// оно не работает
+console.log(a)
