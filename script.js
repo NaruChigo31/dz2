cbf203c20c52c1cc5a9f967dd1fcaba582e1cab8
NaruChigo31 <volomaksim72@gmail.com>
2023-02-17 23:42:29 +0200

    last commit

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function createElem(tag, properties = [], styles = [], parentElem){
 
     let elem = document.createElement(tag);
 
-    parentElemPosition = document.querySelector(parentElem);
+    let parentElemPosition = document.querySelector(parentElem);
+    console.log(parentElem);
     parentElemPosition.append(elem);
+    // document.body.append(elem);
      
     for (let propertie of properties) {
         elem[propertie[0]] = propertie[1]
@@ -14,6 +16,10 @@ function createElem(tag, properties = [], styles = [], parentElem){
     }
     
 }
-
 createElem("input", [["innerHTML", "Lorem ipsum"],["placeholder","pizza"]], [["fontSize", "18px"],["color","red"]], ".coolDiv");
-
+createElem("span",[["class","hello"]] ,[["width", "300px"], ["height","200px"]],"div")
+createElem("h1", [["innerHTML", "Знатоки внимание вопрос, где Андреи?"]], [["fontSize","34px"],["border","2px green solid"], ["border-radius", "3px"]],"span")
+// let b = document.createElement("h1");
+// b.innerHTML = "privet";
+// document.body.append(b);
+// console.log(b.innerHTML)
